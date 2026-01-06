bb54841d8636b2f9abd7247faa04cdb19ea7f359
f-Rra <facundo.rra@gmail.com>
2026-01-06 17:06:51 -0300

    feat: crear interfaz de Login - Diseño con Bootstrap (card centrado) - Campos usuario y contraseña - Validadores ASP.NET - Panel de mensajes de error

---
+++ Gestion-Comercial-Web/Gestion-Comercial-Web/Login.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Web.UI;
+
+namespace Gestion_Comercial_Web
+{
+    public partial class Login : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnIngresar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
