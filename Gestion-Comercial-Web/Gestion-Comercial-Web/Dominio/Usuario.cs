7844f40019aac43c357afe12c407b1fad9b3b703
f-Rra <facundo.rra@gmail.com>
2026-01-05 21:54:48 -0300

    feat: migrar capa de Dominio desde WinForms - Copiar clases de entidades sin modificaciones - Articulo, Categoria, Marca, Usuario, Venta - Reutilización completa de lógica de negocio

---
+++ Gestion-Comercial-Web/Gestion-Comercial-Web/Dominio/Usuario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Usuario
+    {
+        public int IdUsuario { get; set; }
+        public string NombreUsuario { get; set; }
+        public string Contrasena { get; set; }
+        public bool EsAdministrador { get; set; }
+        public bool Estado { get; set; }
+
+        public Usuario()
+        {
+            Estado = true;
+            EsAdministrador = false;
+        }
+    }
+}
